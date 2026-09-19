71c83e1886491f14d46a672d1da0264d383a8224
nerlock1123 <144517234+nerlock1123@users.noreply.github.com>
2026-09-19 17:43:34 +1000

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
   // 'primary' — основной
   // 'backup1' — резервный №1 из backup-key.js
   // 'backup2' — резервный №2 из backup-key-2.js
-  DGIS_KEY_MODE: 'backup1',
+  DGIS_KEY_MODE: 'primary',
 
   DGIS_KEY_PRIMARY: 'e7359306-74a8-4abc-8371-c7f4d85715a0',
 
